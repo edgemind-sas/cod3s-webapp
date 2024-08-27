413791b2dbef1a3e5adcc79968c0e770b2427068
izmar abderrahim <e2305347@etud.univ-ubs.fr>
2024-08-27 13:28:27 +0200

    commit

--- src/config/globals.ts
+++ src/config/globals.ts
@@ -1,6 +1,7 @@
 // src/config/globals.js
+const apiPort = import.meta.env.API_PORT || '8000';
 export default {
-    apiBaseUrl: 'http://localhost:8000/',
+    apiBaseUrl: 'http://localhost:${apiPort}/',
     pathDocument: '/home/abderrahim/Bureau/INTWINE/project/cod3s-webapp/src/assets'
   };
   
