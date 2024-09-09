719816953d64ed29a332f3f9c26f6eb94c9c215e
izmar abderrahim <e2305347@etud.univ-ubs.fr>
2024-09-09 09:41:48 +0200

    mode stand alone with  script modified.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Afficher les variables d'environnement pour le débogage
-echo "BACKEND_PORT is set to: $BACKEND_PORT"
+echo "URL WebApp: https://localhost:$NGINX_SSL_PORT/modelisation"
 
 # Remplacer les variables d'environnement dans nginx.conf.template et les écrire dans nginx.conf
 envsubst '${BACKEND_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
